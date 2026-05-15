250f59223d408edee84ca806f9f5531209502457
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-16 01:49:10 +0700

    Time: 0 ms (100%), Space: 53 MB (86.21%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
 var numIdenticalPairs = function(nums) {
-    const myMap = new Map();
-    for(let i=0;i<nums.length;i++)
-        if(myMap.has(nums[i])) myMap.set(nums[i], myMap.get(nums[i])+1);
-        else myMap.set(nums[i],1);
-    return Array.from(myMap.values()).reduce((count,x)=> x>1?count+=x*(x-1)/2:count ,0);
+    let count=0, newArray=[];
+    for (let x of nums){
+        count+= (newArray[x] || 0);
+        newArray[x] = (newArray[x] || 0) + 1;
+    }
+    return count;
 };
