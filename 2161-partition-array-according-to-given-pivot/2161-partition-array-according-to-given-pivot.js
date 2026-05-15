ddee8e6db54df25e61123474628f19e22fbab049
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-16 03:17:07 +0700

    Time: 43 ms (14.02%), Space: 81.8 MB (98.78%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
@@ -4,9 +4,14 @@
  * @return {number[]}
  */
 var pivotArray = function(nums, pivot) {
-    let before=[], between=[], after=[];
-    for(let x of nums)
-        x<pivot?before.push(x):x===pivot?between.push(x):after.push(x);
-    //return [...before, ...between, ...after];
-    return before.concat(between,after);
+    let length=nums.length, left=0, right=length-1, result=[];
+    for(let i=0, j=length-1;i<length;i++,j--){
+        if (nums[i]<pivot)
+            result[left++]=nums[i];
+        if (nums[j]>pivot)
+            result[right--]=nums[j];
+    }
+    while (left<=right)
+        result[left++]=pivot;
+    return result;
 };
