9ce4b003ede73c25ae0acab81064336e3f4f52f3
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-16 02:41:55 +0700

    Time: 1 ms (93.78%), Space: 57.1 MB (69.29%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.js
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.js
@@ -3,10 +3,10 @@
  * @return {number[]}
  */
 var transformArray = function(nums) {
-    let even=0, odd=0, result=[];
+    let j=0;
     for (let x of nums)
-        x%2===0?even++:odd++;
-    for (let i=0;i<nums.length;i++)
-        even>0?result.push(0) && even--:result.push(1) && odd--;
-    return result;
+        if(x%2===0) nums[j++]=0;
+    while(j<nums.length)
+        nums[j++]=1;
+    return nums;
 };
