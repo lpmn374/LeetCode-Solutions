0478fd1642e5756ec680b66058c0b5d3110785f2
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-17 00:45:02 +0700

    Time: 0 ms (100%), Space: 54.1 MB (45.04%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.js
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.js
@@ -3,8 +3,12 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let left=0, right=nums.length -1;
-    while(nums[left]>=nums[right] && left<right)
-        left++;
+    let left=0, right=nums.length -1, mid=0;
+    while(left<right){
+        mid=left+Math.floor((right-left)/2);
+        if (nums[mid]>nums[right]) left=mid+1;
+        else if (nums[mid]<nums[right]) right=mid;
+        else right--;
+    }
     return nums[left];
 };
