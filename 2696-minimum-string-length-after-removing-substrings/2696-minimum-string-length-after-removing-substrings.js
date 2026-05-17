630337930aa2e9674bb1e20fcfd190d0b1fcfdeb
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-18 01:28:16 +0700

    Time: 2 ms (91.07%), Space: 57.9 MB (52.68%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.js
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.js
@@ -2,30 +2,14 @@
  * @param {string} s
  * @return {number}
  */
-class Node{
-    constructor(data){
-        this.data=data;
-        this.next=null;
-    }
-}
 var minLength = function(s) {
-    let start = new Node(), current = start, length=0;
-    for(let i=0;i<s.length;i++){
-        current.next=new Node(s[i]);
-        current=current.next;
-    }
-    current=start;
-    while(current.next!==null && current.next.next!==null)
-        if ((current.next.data==='A'&&current.next.next.data==='B')||(current.next.data==='C'&&current.next.next.data==='D')){
-            current.next=current.next.next.next;
-            current=start;
-        }
-        else current=current.next;
-    current=start;
-    while(current.next!=null){
-        length++;
-        current=current.next;
-    }
-    return length;
+    let stack = [];
+    for(let i=0;i<s.length;i++)
+        if(s[i]==='B' && stack[stack.length-1]==='A')
+            stack.pop();
+        else if(s[i]==='D' && stack[stack.length-1]==='C')
+            stack.pop();
+        else
+            stack.push(s[i]);
+    return stack.length;
 };
-
