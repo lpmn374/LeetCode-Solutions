a05b39aba19263d906272fe827b84d13aebaa27a
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-19 00:05:30 +0700

    Time: 1 ms (96.13%), Space: 67.2 MB (36.46%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+    let isIncreasing=true, isDecreasing=true;
+    for(let i=0;i<nums.length-1;i++){
+        if(nums[i]>nums[i+1]) isIncreasing=false;
+        if(nums[i]<nums[i+1]) isDecreasing=false;
+        if(!isIncreasing && !isDecreasing) return false;
+    }
+    return isIncreasing || isDecreasing;
+};
