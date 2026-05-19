21435299c1f56c736d3fec48961eba9479f6970e
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-20 00:54:16 +0700

    Time: 2 ms (52.88%), Space: 63.5 MB (43.27%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var getCommon = function(nums1, nums2) {
-    let length=Math.max(nums1.length, nums2.length), delete1=0, delete2=0, i=0;
+    let delete1=0, delete2=0, i=0;
     while (i+delete1>=0){
         if(!nums1[i+delete1] || !nums2[i+delete2]) return -1;
         if(nums1[i+delete1]===nums2[i+delete2]) return nums1[i+delete1];
