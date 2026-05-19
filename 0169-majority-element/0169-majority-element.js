993bc2d8323aaf727d52ba2cae6bd035407fe08a
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-19 23:42:19 +0700

    Time: 8 ms (19.18%), Space: 58.8 MB (22.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,6 +3,9 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    nums.sort((a,b)=>a-b);
-    return nums[Math.floor(nums.length/2)];
+    const hashMap = new Map();
+    for(let x of nums)
+        hashMap.set(x, (hashMap.get(x)||0)+1);
+    for(let [key, value] of hashMap)
+        if(value>Math.floor(nums.length/2)) return key;
 };
