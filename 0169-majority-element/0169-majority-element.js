6b958a021651eeb63b26c3523643acb7bf7ca644
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-19 23:30:09 +0700

    Time: 5 ms (42.8%), Space: 58.5 MB (24.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -4,12 +4,5 @@
  */
 var majorityElement = function(nums) {
     nums.sort((a,b)=>a-b);
-    let time;
-    while(true){
-        time=Math.floor(nums.length/2);
-        if(nums[0]===nums[time] || nums[time]===nums[nums.length-1])
-            return nums[time];
-        else
-            nums.pop();
-    }
+    return nums[Math.floor(nums.length/2)];
 };
