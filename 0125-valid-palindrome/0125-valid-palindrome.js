2b728f404749a01514b087b472a07f705036e166
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-21 15:31:33 +0700

    Time: 5 ms (63.65%), Space: 57.9 MB (57.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -8,10 +8,7 @@ var isPalindrome = function(s) {
         if ((s[i].charCodeAt(0)>='a'.charCodeAt(0) && s[i].charCodeAt(0)<='z'.charCodeAt(0)) || (s[i].charCodeAt(0)>='0'.charCodeAt(0) && s[i].charCodeAt(0)<='9'.charCodeAt(0)) ) arr.push(s[i]);
         else if (s[i].charCodeAt(0)>='A'.charCodeAt(0) && s[i].charCodeAt(0)<='Z'.charCodeAt(0)) arr.push(s[i].toLowerCase());
         let left=0, right=arr.length-1;
-        while(left<right){
-            if(arr[left]!==arr[right]) return false;
-            left++;
-            right--;
-        }
-        return true;
+    while(left<right)
+        if(arr[left++]!==arr[right--]) return false;
+    return true;
 };
