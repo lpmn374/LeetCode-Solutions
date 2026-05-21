2648e9233b6133077ae6ed182567679db8a4615e
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-21 16:19:40 +0700

    Time: 5 ms (33.79%), Space: 55.7 MB (54.67%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -4,10 +4,10 @@
  */
 var isValid = function(s) {
     if(s.length%2!==0) return false;
-    const myMap=new Map ([['(',')'],['{','}'],['[',']'],[')',-1],['}',-1],[']',-1]]);
+    const myMap=new Map ([['(',')'],['{','}'],['[',']']]);
     let stack=[];
     for(let x of s)
-        if (myMap.get(x)!==-1) stack.push(x);
+        if (myMap.has(x)) stack.push(x);
         else if (stack.length===0 || x!==myMap.get(stack[stack.length-1])) return false;
         else stack.pop();
     if (stack.length===0) return true;
