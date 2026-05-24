7d989ce5a5d5d166022d78aaadc2b223ca7bbaa3
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-25 03:55:59 +0700

    Time: 2 ms (82.41%), Space: 69 MB (51.85%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -4,14 +4,14 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
-    if(nums.length<=k) return nums.reduce((acc,cur)=>acc+cur,0)/k;
+    if(k===nums.length) return nums.reduce((acc,cur)=>acc+cur,0)/k;
     let sum=0;
     for(let i=0;i<k;i++)
         sum+=nums[i];
     let maxSum=sum;
-    for(let j=k;j<=nums.length-1;j++){
-        sum+= nums[j] - nums[j-k];
-        if (sum>maxSum) maxSum=sum;
+    for(let i=k;i<nums.length;i++){
+        sum+=nums[i]-nums[i-k];
+        maxSum=Math.max(sum,maxSum);
     }
     return maxSum/k;
 };
