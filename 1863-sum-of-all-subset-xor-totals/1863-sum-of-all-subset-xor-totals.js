a94ba6ef12ae0cabf087f15ef8969bac534cbac7
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-25 03:09:05 +0700

    Time: 4 ms (53.7%), Space: 52.7 MB (96.3%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.js
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var subsetXORSum = function(nums) {
-    let result=[0], current_subset=[];
-    backtrack(0, current_subset, nums, result);
-    return result[0];
-};
-function backtrack(start, current_subset, nums, result){
-    result[0]+=current_subset.reduce((result,x)=>result^x,0);
-    for (let i=start;i<nums.length;i++){
-        current_subset.push(nums[i]);
-        backtrack(i + 1, current_subset, nums, result);
-        current_subset.pop();
+    let current=[], result=0;
+    function backtracking(start, current){
+        result+=current.reduce((acc,cur)=>acc^cur,0);
+        for(let i=start;i<nums.length;i++){
+                current.push(nums[i]);
+                backtracking(i+1, current);
+                current.pop();
+        }
     }
-}
+    backtracking(0,[]);
+    return result;
+};
