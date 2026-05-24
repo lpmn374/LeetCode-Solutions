d6086f21e839e64c5ed7bea5211010d529b4d1f8
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-25 04:01:56 +0700

    Time: 0 ms (100%), Space: 53.3 MB (70.74%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -4,10 +4,10 @@
  */
 var rob = function(nums) {
     if(nums.length===1) return nums[0];
-    let dp=new Array(nums.length), max=0;
+    let dp=new Array(nums.length);
     dp[0]=nums[0];
     dp[1]=Math.max(nums[0],nums[1]);
     for(let i=2;i<nums.length;i++)
         dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
-    return dp[nums.length-1];
+    return dp[dp.length-1];
 };
