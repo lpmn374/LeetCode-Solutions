d5fa8eb9f5bdbbe6a13314a0a5211af0f1e39bc9
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-26 01:36:49 +0700

    Time: 1 ms (96.65%), Space: 62.8 MB (72.68%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.js
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.js
@@ -3,8 +3,7 @@
  * @return {number[]}
  */
 var findDegrees = function(matrix) {
-    let result=[];
     for(let i=0;i<matrix.length;i++)
-        result.push(matrix[i].reduce((acc,cur)=>acc+cur,0));
-    return result;
+        matrix[i]=matrix[i].reduce((acc,cur)=>acc+cur,0);
+    return matrix;
 };
