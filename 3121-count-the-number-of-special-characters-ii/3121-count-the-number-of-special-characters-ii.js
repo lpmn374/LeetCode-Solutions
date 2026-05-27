535d7d080c0aa79ec15e9e1eadbfbea849d023fc
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-05-27 22:23:38 +0700

    Time: 77 ms (73.91%), Space: 62 MB (82.61%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.js
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var numberOfSpecialChars = function(word) {
-    let lower=new Array(26).fill(0), upper=new Array(26).fill(0), firstLower='a'.charCodeAt(0), firstUpper='A'.charCodeAt(0), count=0;
+    let arr=new Array(26).fill(0), firstLower='a'.charCodeAt(0), firstUpper='A'.charCodeAt(0), count=0;
     for(let i=0;i<word.length;i++){
         let letter=word[i].charCodeAt(0);
-        if(word[i]>='A' && word[i]<='Z'){
-            upper[letter-firstUpper]=1;
-            if(lower[letter-firstUpper]===0) lower[letter-firstUpper]=-1;
-        }
-        if (word[i]>='a' && word[i]<='z' && upper[letter-firstLower]===0)
-            lower[letter-firstLower]=1;
-        else lower[letter-firstLower]=-1;
+        if(word[i]>='a' && word[i]<='z')
+            if(arr[letter-firstLower]===0 || arr[letter-firstLower]===1) arr[letter-firstLower]=1; 
+            else arr[letter-firstLower]=-1;
+        else if(word[i]>='A' && word[i]<='Z')
+            if(arr[letter-firstUpper]===0) arr[letter-firstUpper]=-1;
+            else if(arr[letter-firstUpper]===1) arr[letter-firstUpper]=2;
     }
-    for(let i=0;i<lower.length;i++)
-        if(lower[i]===1 && upper[i]===1)
-            count++;
+    for(let i=0;i<arr.length;i++)
+        if(arr[i]===2) count++;
     return count;
 };
