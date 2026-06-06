99c9d910eed4fb4fee95caa5b1d6faef323c03b5
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-06 20:02:30 +0700

    Time: 1 ms (92.48%), Space: 58.4 MB (26.99%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -3,14 +3,12 @@
  * @return {number[]}
  */
 var leftRightDifference = function(nums) {
-    let result=[], left=0, right=0;
-    for(let i=0;i<nums.length;i++){
-        result.push(left);
-        left+=nums[i];
+    let arr=[0], rightSum=0;
+    for(let i=0;i<nums.length-1;i++)
+        arr.push(arr[i]+nums[i]);
+    for(let i=arr.length-1;i>=0;i--){
+        arr[i]=Math.abs(arr[i]-rightSum);
+        rightSum+=nums[i];
     }
-    for(let i=nums.length-1;i>=0;i--){
-        result[i]=Math.abs(result[i]-right);
-        right+=nums[i];
-    }
-    return result;
+    return arr;
 };
