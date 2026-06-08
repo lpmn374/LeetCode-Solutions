6a38cbcdc1e66092fe91f384fb2dea8d73030536
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-08 23:36:50 +0700

    Time: 8 ms (46.98%), Space: 65 MB (32.46%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x<0) return false;
+    x=x.toString();
+    let left=0, right=x.length-1;
+    while(left<right)
+        if(x[left++]!==x[right--]) return false;
+    return true;
+};
