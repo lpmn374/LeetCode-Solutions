bcd3642d4087bce545b89a7063768b1030e082cd
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-14 01:36:26 +0700

    Time: 8 ms (94.74%), Space: 67.6 MB (97.37%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.js
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.js
@@ -6,11 +6,12 @@
 var garbageCollection = function(garbage, travel) {
     let meetSet= new Set(), travelTime=travel.reduce((acc,cur)=>acc+cur,0), totalTime=0;
     for(let i=garbage.length-1;i>=0;i--){
-        for(let j=0;j<garbage[i].length;j++)
-            if (!meetSet.has(garbage[i][j])){
-                meetSet.add(garbage[i][j]);
-                totalTime+=travelTime;
-            }
+        if(meetSet.size<3)
+            for(let j=0;j<garbage[i].length;j++)
+                if (!meetSet.has(garbage[i][j])){
+                    meetSet.add(garbage[i][j]);
+                    totalTime+=travelTime;
+                }
         totalTime+=garbage[i].length;
         travelTime-=travel[i-1];
     }
