6641b14c154e0a5cab866d3e74a5417867bdf61e
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-15 00:36:41 +0700

    Time: 1 ms (75.47%), Space: 60.8 MB (33.62%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,7 +3,7 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    if (nums.length===1) return nums;
+    if (nums.length===1) return;
     let pointer=0;
     for(let i=1;i<nums.length;i++){
         if(nums[i]!==0 && nums[pointer]===0 && i!==pointer){
@@ -12,7 +12,6 @@ var moveZeroes = function(nums) {
             nums[pointer]=tmp;
             pointer++;
         }
-        if(nums[i]===0 && nums[pointer]!==0) pointer=i;
+        if (nums[pointer]!==0) pointer++;
     }
-    return nums;
 };
