f5f490ad77a39cff7de458bd890a4b8257f15de0
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-16 22:41:59 +0700

    Time: 57 ms (51.85%), Space: 74.7 MB (51.85%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.js
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.js
@@ -4,21 +4,10 @@
  */
 var processStr = function(s) {
     let result=[];
-    for(let i=0;i<s.length;i++){
+    for(let i=0;i<s.length;i++)
         if(s[i]>='a' && s[i]<='z') result.push(s[i]);
         else if (s[i]==='*' && result.length>0) result.pop();
         else if (s[i]==='#') result=result.concat(result);
-        else if (s[i]==='%'){
-            let left=0, right=result.length-1;
-            while(left<right){
-                let tmp=result[left];
-                result[left]=result[right];
-                result[right]=tmp;
-                left++;
-                right--;
-            }
-        }
-        console.log(result);
-    }
+        else if (s[i]==='%') result.reverse();
     return result.join('');
 };
