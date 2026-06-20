4f8ca0d6aee4e929db094b07a07dda8913b90824
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-21 00:39:31 +0700

    Time: 0 ms (100%), Space: 65 MB (10.53%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.js
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.js
@@ -4,11 +4,5 @@
  * @return {number[][]}
  */
 var sortTheStudents = function(score, k) {
-    let arr=[], result=[];
-    for(let i=0;i<score.length;i++)
-        arr.push({key: score[i][k], val: score[i]});
-    arr.sort((a,b)=>b.key-a.key);
-    for(let i=0;i<arr.length;i++)
-        result.push(arr[i].val);
-    return result;
+    return score.sort((a,b)=>b[k]-a[k]);
 };
