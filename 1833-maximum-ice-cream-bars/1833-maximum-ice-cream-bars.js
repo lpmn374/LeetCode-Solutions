030421f0a6a9a8c64bab9460af560d95808eac77
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-21 21:19:58 +0700

    Time: 17 ms (86.36%), Space: 72.3 MB (9.09%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -4,12 +4,25 @@
  * @return {number}
  */
 var maxIceCream = function(costs, coins) {
-    let count=0, i=0;
-    costs.sort((a,b)=>a-b);
-    while(coins>=costs[i]){
-        count++;
-        coins-=costs[i];
-        i++;
+    let max=costs[0], min=costs[0];
+    for(let i=1;i<costs.length;i++){
+        if(costs[i]>max) max=costs[i];
+        if(costs[i]<min) min=costs[i];
+    }
+    let arr=new Array(max-min+1).fill(0);
+    for (let c of costs) 
+        arr[c - min]++;
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let price = i + min; 
+        let quantity = arr[i]; 
+        if (quantity > 0) {
+            let canAfford = Math.floor(coins / price);
+            let take = Math.min(quantity, canAfford);
+            count += take;
+            coins -= take * price;
+            if (take < quantity) break;
+        }
     }
     return count;
 };
