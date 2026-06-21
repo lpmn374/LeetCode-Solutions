69378f21faf6ff233c4c58481998547ae7eaafe3
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-21 20:55:41 +0700

    Time: 134 ms (9.09%), Space: 69.5 MB (29.55%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -6,13 +6,10 @@
 var maxIceCream = function(costs, coins) {
     let count=0, i=0;
     costs.sort((a,b)=>a-b);
-    while(coins>0){
-        if (coins>=costs[i]){
-            count++;
-            coins-=costs[i];
-            i++;
-        }
-        else break;
+    while(coins>=costs[i]){
+        count++;
+        coins-=costs[i];
+        i++;
     }
     return count;
 };
