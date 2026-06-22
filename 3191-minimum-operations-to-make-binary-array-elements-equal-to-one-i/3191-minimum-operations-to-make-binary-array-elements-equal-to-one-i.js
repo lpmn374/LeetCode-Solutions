f792fe41fbe370892c15a3fa8305ea1810cb8e23
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-22 22:24:41 +0700

    Time: 6 ms (95.56%), Space: 65.1 MB (55.56%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
@@ -7,8 +7,8 @@ var minOperations = function(nums) {
     for(let i=0;i<nums.length;i++)
         if(nums[i]===0 && i+2<nums.length){
             nums[i]=1;
-            nums[i+1]===1?nums[i+1]=0:nums[i+1]=1;
-            nums[i+2]===1?nums[i+2]=0:nums[i+2]=1;
+            nums[i+1]^=1;
+            nums[i+2]^=1;
             count++;
         }
         else if (nums[i]===0 && i+2>=nums.length) return -1;
