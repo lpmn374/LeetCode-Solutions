c5d0c9c9379919742ccaab3f04ec8edfa6719c31
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-22 22:05:51 +0700

    Time: 1 ms (56.56%), Space: 54.7 MB (91.8%) - LeetHub

---
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countPartitions = function(nums) {
+    let left=0, right=0, count=0;
+    for(let i=1;i<nums.length;i++)
+        right+=nums[i];
+    for(let i=0;i<nums.length-1;i++){
+        left+=nums[i];
+        if((left-right)%2===0) count++;
+        if (i<nums.length-1) right-=nums[i+1];
+    }
+    return count;
+};
