8f0c273ce4248ea2ad1c0b95a164174b10b4e697
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-23 23:53:48 +0700

    Time: 0 ms (100%), Space: 54.1 MB (52.78%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.js
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.js
@@ -3,19 +3,8 @@
  * @return {string}
  */
 var findDifferentBinaryString = function(nums) {
-    let mySet=new Set(), number=0, result=[];
-    for(let i=0;i<nums.length;i++){
-        let current=0;
-        for(let j=0;j<nums[i].length;j++)
-            current+=nums[i][j]*(2**(nums[i].length-1-j));
-        mySet.add(current);
-    }
-    while (mySet.has(number)) number++;
-    for(let i=0;i<nums[0].length;i++)
-        if(number>=2**(nums[0].length-1-i)){
-            result.push(1);
-            number-=2**(nums[0].length-1-i);
-        }
-        else result.push(0);
-    return result.join('');
+    let result="";
+    for(let i=0;i<nums.length;i++)
+        result += nums[i][i]==='0'?'1':'0';
+    return result;
 };
