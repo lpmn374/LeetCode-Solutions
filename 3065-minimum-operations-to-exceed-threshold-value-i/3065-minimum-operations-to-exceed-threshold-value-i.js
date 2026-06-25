4f78c92582b128bf8921d2a11f1aab47e9f16bb1
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-25 23:28:05 +0700

    Time: 0 ms (100%), Space: 55 MB (89.23%) - LeetHub

--- 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.js
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.js
@@ -4,8 +4,8 @@
  * @return {number}
  */
 var minOperations = function(nums, k) {
-    let count=0;
-    for(let i=0;i<nums.length;i++)
+    let count=0, n=nums.length;
+    for(let i=0;i<n;i++)
         if(nums[i]<k) count++;
     return count;
 };
