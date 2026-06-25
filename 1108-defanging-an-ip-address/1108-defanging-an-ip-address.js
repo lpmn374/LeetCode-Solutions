c193e8905e53772141811b5615fdb6944db15f77
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-25 23:51:37 +0700

    Time: 52 ms (7.3%), Space: 54.1 MB (12.92%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -4,8 +4,10 @@
  */
 var defangIPaddr = function(address) {
     let n=address.length, result="";
-    for(let i=0;i<n;i++)
-        if(address[i]==='.') result+='[.]';
-        else result+=address[i];
+    for(let i=0;i<n;i++){
+        let char=address[i];
+        if(char==='.') result+='[.]';
+        else result+=char;
+    }
     return result;
 };
