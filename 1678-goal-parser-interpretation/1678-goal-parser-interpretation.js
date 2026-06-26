24b3cb7a3a892cf7245ff58d6a0462775e82ba2f
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-26 23:01:47 +0700

    Time: 48 ms (33.65%), Space: 53.8 MB (50%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -3,12 +3,5 @@
  * @return {string}
  */
 var interpret = function(command) {
-    let n=command.length, result="";
-    for(let i=0;i<n;i++){
-        if (command[i]===')') continue;
-        if (command[i]==='(' && command[i+1]!==')') continue;
-        if(command[i]==='(' && command[i+1]===')') result+='o';
-        else result+=command[i];
-    }
-    return result;
+    return command.split('()').join('o').split('(al)').join('al');
 };
