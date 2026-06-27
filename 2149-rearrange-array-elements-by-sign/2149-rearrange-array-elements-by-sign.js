c6d3d77382bab2382bbe38a30208cb172e8de908
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-06-27 23:15:08 +0700

    Time: 24 ms (33.13%), Space: 98 MB (53.7%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
@@ -3,22 +3,15 @@
  * @return {number[]}
  */
 var rearrangeArray = function(nums) {
-    let n=nums.length, pos=0, neg=0, result=[];
-    while (nums[pos]<0) pos++;
-    while (nums[neg]>0) neg++;
-    while(result.length<n){
-        if (pos!==-1){
-            result.push(nums[pos]);
-            pos++;
+    let n=nums.length, pos=0, neg=1, result=[];
+    for(let i=0;i<n;i++)
+        if(nums[i]>0){
+            result[pos]=nums[i];
+            pos+=2;
         }
-        if (neg!==-1){
-            result.push(nums[neg]);
-            neg++;
+        else{
+            result[neg]=nums[i];
+            neg+=2;
         }
-        while (nums[pos]<0 && pos<n) pos++;
-        while (nums[neg]>0 && neg<n) neg++;
-        if (pos===n) pos=-1;
-        if (neg===n) neg=-1;
-    }
     return result;
 };
