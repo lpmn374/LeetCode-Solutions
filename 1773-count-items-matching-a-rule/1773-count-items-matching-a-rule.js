a13be22df5740fd6c5e048a0bfeec48e304741c1
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-07-01 00:18:35 +0700

    Time: 1 ms (61.9%), Space: 60 MB (31.43%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
@@ -5,11 +5,11 @@
  * @return {number}
  */
 var countMatches = function(items, ruleKey, ruleValue) {
-    let index, search=new Map();
+    let index, count=0;
     if (ruleKey==='type') index=0;
     else if (ruleKey==='color') index=1;
     else index=2;
     for(let i=0;i<items.length;i++)
-        search.set(items[i][index], (search.get(items[i][index]) || 0) +1 );
-    return search.get(ruleValue)||0;
+        if(items[i][index]===ruleValue) count++;
+    return count;
 };
