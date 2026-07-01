25c8b821517e81381bfbb1a121a9dee175b63dd8
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-07-02 01:30:32 +0700

    Time: 4 ms (6.56%), Space: 57.1 MB (18.03%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.js
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -3,10 +3,12 @@
  * @return {number}
  */
 var scoreOfString = function(s) {
-    let object={}, n=s.length, first='a'.charCodeAt(0), sum=0;
-    for(let i=0;i<26;i++)
-        object[String.fromCharCode(i+first)]= i+first;
-    for(let j=0;j<n-1;j++)
-        sum+=Math.abs(object[s[j]]-object[s[j+1]]);
+    let object={}, n=s.length, sum=0;
+    for(let i=0;i<n-1;i++){
+        if(!object[s[i]]) object[s[i]]=s.charCodeAt(i);
+        if(!object[s[i+1]]) object[s[i+1]]=s.charCodeAt(i+1);
+        let current=object[s[i]], next=object[s[i+1]];
+        sum+=Math.abs(current-next);
+    }
     return sum;
 };
