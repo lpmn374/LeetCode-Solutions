11630ba6ee24ff7ebc4e07ea8a21d74741dc41b9
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-07-02 02:11:06 +0700

    Time: 4 ms (60.54%), Space: 57.3 MB (36.85%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -3,27 +3,16 @@
  * @return {string}
  */
 var removeOuterParentheses = function(s) {
-    let n=s.length, isOpen=false, rmIndex=[], balance=0, result="";
-    for(let i=0;i<n;i++){
-        if (!isOpen && s[i]==='('){ 
-            isOpen=true;
-            rmIndex.push(i);
+    let balance=0, result="";
+    for(let c of s){
+        if (c==="("){
+            if (balance>0) result+=c;
+            balance++;
         }
-        else if(s[i]==="(") balance++;
-        else balance--;
-        if (balance<0){
-            isOpen=false;
-            rmIndex.push(i);
-            balance=0;
+        else{
+            balance--;
+            if (balance>0) result+=c;
         }
-    }
-    let current=0;
-    for(let j=0;j<n;j++){
-        if(j===rmIndex[current]){
-            current++;
-            continue;
-        }
-        result+=s[j];
     }
     return result;
 };
