16803b2efd44e255ab6b73e4b92652a097f87a05
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-07-03 23:51:45 +0700

    Time: 0 ms (100%), Space: 54 MB (44.37%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,11 +4,11 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    let isFound=false;
-    for(let start=0; start<haystack.length; start++)
-        for(let i=0;i<needle.length;i++){
+    if(!haystack.includes(needle)) return -1;
+    let isFound=false, hLen=haystack.length, nLen=needle.length;
+    for(let start=0; start<hLen; start++)
+        for(let i=0;i<nLen;i++){
             if(haystack[start+i]!==needle[i]) break;
             if(i===needle.length-1) return start;
         }
-    return -1;
 };
