e7363955c5799543696a158ee2f88b8ca5f16772
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-07-04 23:58:44 +0700

    Time: 8 ms (36.96%), Space: 59.8 MB (40.22%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.js
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.js
@@ -4,14 +4,10 @@
  * @return {number[]}
  */
 var findIntersectionValues = function(nums1, nums2) {
-    let myNum1=new Map(), myNum2=new Set(), sum1=0, sum2=0, n1Len=nums1.length, n2Len=nums2.length;
+    let set1=new Set(nums1), set2=new Set(nums2), sum1=0, sum2=0, n1Len=nums1.length, n2Len=nums2.length;
     for(let i=0;i<n1Len;i++)
-        myNum1.set(nums1[i], (myNum1.get(nums1[i])||0)+1);
-    for(let i=0;i<n2Len;i++){
-        myNum2.add(nums2[i]);
-        if (myNum1.has(nums2[i])) sum2++;
-    }
-    for(let [x,y] of myNum1)
-        if(myNum2.has(x)) sum1+=y;
+        if (set2.has(nums1[i])) sum1++;
+    for(let i=0;i<n2Len;i++)
+        if (set1.has(nums2[i])) sum2++;
     return [sum1,sum2];
 };
