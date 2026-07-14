dd18a1b87715697cdd0378483771d6b17ae87849
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-07-14 22:06:46 +0700

    Time: 33 ms (94.39%), Space: 59.4 MB (95.44%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    nums.sort((a,b) => a-b);
+    let sum = 0;
+    for(let i = 0; i < nums.length; i = i + 2)
+        sum += nums[i];
+    return sum;
+};
