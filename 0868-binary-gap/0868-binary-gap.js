64122b4c5aab115d1b084cb306d6af472d2b7866
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-07-21 23:45:36 +0700

    Time: 15 ms (2.82%), Space: 59.2 MB (7.04%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var binaryGap = function(n) {
+    let bit=new Array(31).fill(0), start=30;
+    while(n>0){
+        if(2**start<=n){
+            bit[30-start]=1;
+            n-=2**start;
+        }
+        start--;
+    }
+    console.log(bit);
+    let flag=0, current=0, max=-Infinity, a=null, b=null;
+    for(let i=0;i<31;i++){
+        if(bit[i]===1 && flag===0){
+            a=i;
+            flag=1;
+        }
+        else if (bit[i]===1 && flag===1){
+            b=i;
+            flag=0;
+        }
+        
+        if (a && b) {
+            current=Math.abs(a-b);
+            if(max<current) max=current;
+        }
+    }
+    return max!==-Infinity?max:0;
+};
