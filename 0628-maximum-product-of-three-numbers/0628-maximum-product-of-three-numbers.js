9286568d1e0867662de8a23c949278bd42fb0fd4
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-07-26 23:46:11 +0700

    Time: 38 ms (33.93%), Space: 60.5 MB (10.27%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -3,17 +3,18 @@
  * @return {number}
  */
 var maximumProduct = function(nums) {
-    if (nums.length===3) return nums[0]*nums[1]*nums[2];
+    let n=nums.length;
+    if (n===3) return nums[0]*nums[1]*nums[2];
     nums.sort((a,b)=> a-b);
-    if (nums[0]<0 && nums[1]<0 && nums[nums.length-1]>0) 
-        if(nums[0]*nums[1] > nums[nums.length-3]*nums[nums.length-2])
-            return nums[0]*nums[1]*nums[nums.length-1];
-    if (nums[nums.length-1]>0 && nums[nums.length-2]>0 && nums[nums.length-3]<0){
-        end=nums.length-2;
+    if (nums[0]<0 && nums[1]<0 && nums[n-1]>0) 
+        if(nums[0]*nums[1] > nums[n-3]*nums[n-2])
+            return nums[0]*nums[1]*nums[n-1];
+    if (nums[n-1]>0 && nums[n-2]>0 && nums[n-3]<0){
+        end=n-2;
         start=end-2;
     }
     else{
-        end=nums.length-1;
+        end=n-1;
         start=end-2;
     }
     let result=nums[start];
