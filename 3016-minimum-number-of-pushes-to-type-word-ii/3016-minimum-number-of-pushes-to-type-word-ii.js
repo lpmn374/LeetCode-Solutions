ec5d576d127a675a4563aabe13c98a0a4808a6b0
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-07-31 21:57:31 +0700

    Time: 9 ms (100%), Space: 62.6 MB (72.73%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.js
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.js
@@ -3,17 +3,15 @@
  * @return {number}
  */
 var minimumPushes = function(word) {
-    let myMap=new Map();
-    for(let i=0;i<word.length;i++)
-        myMap.set(word[i], (myMap.get(word[i])||0)+1);
-    if(myMap.size<9) return word.length;
-    let arr=Array.from(myMap.entries());
-    arr.sort((a,b)=>b[1]-a[1]); //0-7,8-15,16-23
-    let click=1, sum=0, count=0;
-    for(let [key,val] of arr){
-        sum+=val*click;
-        count++;
-        if(count%8===0) click++;
+    let count=new Array(26).fill(0), n=word.length, a='a'.charCodeAt(0);
+    for(let i=0;i<n;i++)
+        count[word.charCodeAt(i)-a]++;
+    count.sort((a,b)=>b-a);
+    let turn=1, sum=0;
+    for(let i=0;i<26;i++){
+        if(count[i]===0) break;
+        if(i>0 && i%8===0) turn++;
+        sum+=count[i]*turn;
     }
     return sum;
 };
