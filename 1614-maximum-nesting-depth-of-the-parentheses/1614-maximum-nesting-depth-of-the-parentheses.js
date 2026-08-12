8ce5bcde6f133126b5ccd2ebe78bc387fcc4afe5
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-08-12 20:57:34 +0700

    Time: 0 ms (100%), Space: 55.5 MB (5.22%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
@@ -6,8 +6,10 @@ var maxDepth = function(s) {
     let count=0, max=0;
     for(let i=0;i<s.length;i++){
         if(s[i]==='(') count++;
-        else if (s[i]===')') count--;
-        if (count>max) max=count;
+        else if (s[i]===')'){
+            if (count>max) max=count;
+            count--;
+        }  
     }
     return max;
 };
