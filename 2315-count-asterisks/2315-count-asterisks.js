7c77d1290ba1a234269b1e6ceec7857f96d6b27f
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-09-04 23:39:41 +0700

    Time: 1 ms (76.81%), Space: 53 MB (95.65%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.js
+++ 2315-count-asterisks/2315-count-asterisks.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var countAsterisks = function(s) {
-    let isOpen=false, count=0, n=s.length;
+    let isOpen=0, count=0, n=s.length;
     for(let i=0;i<n;i++){
-        if(s[i]==='|')
-            if (isOpen) isOpen=false;
-            else isOpen=true;
-        if(s[i]==='*' && !isOpen) count++;
+        if(s[i]==='|') isOpen^=1;
+        if(s[i]==='*' && isOpen===0) count++;
     }
     return count;
 };
