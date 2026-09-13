26e18e77d1fb247f1edf23710f9178957041d2aa
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-09-13 21:55:23 +0700

    Time: 1 ms (100%), Space: 57.3 MB (66.22%) - LeetHub

--- 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.js
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.js
@@ -3,13 +3,15 @@
  * @return {string}
  */
 var reverseByType = function(s) {
-    let n=s.length, arrL=[], arrS=[];
-    for(let i=n-1;i>=0;i--)
-        if(s[i]>='a' && s[i]<='z') arrL.push(s[i]);
-        else arrS.push(s[i]);
-    let indexL=0, indexS=0, result=[];
+    let n=s.length, pL=n-1, pS=n-1, result="";
     for(let i=0;i<n;i++)
-        if(s[i]>='a' && s[i]<='z') result.push(arrL[indexL++]);
-        else result.push(arrS[indexS++]);
-    return result.join('');
+        if(s[i]>='a' && s[i]<='z'){
+            while(!(s[pL]>='a' && s[pL]<='z')) pL--;
+            result+=s[pL--];
+        }
+        else{
+            while(s[pS]>='a' && s[pS]<='z') pS--;
+            result+=s[pS--];
+        }
+    return result;
 };
