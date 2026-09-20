55a0ed1c1c1fffdce5becac8fcdf970ef64fcbe7
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-09-20 23:10:03 +0700

    Time: 1 ms (100%), Space: 44.1 MB (50.47%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int sum=0;
+        for(int i=0;i<s.length();i++)
+            sum+= ('z' - s.codePointAt(i) +1)*(i+1);
+        return sum;
+    }
+}
