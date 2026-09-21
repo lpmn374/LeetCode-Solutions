eda894b963a271c2537e7a1339143169b55d94d6
Ocleng <151450732+lpmn374@users.noreply.github.com>
2026-09-21 22:36:44 +0700

    Time: 1 ms (55.88%), Space: 43.3 MB (7.11%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countPoints(String rings) {
+        Map<Character, Set<Character>> map1 = new HashMap<>();
+        int count=0;
+        for(int i=1;i<rings.length();i+=2){
+            map1.putIfAbsent(rings.charAt(i), new HashSet<>());
+            map1.get(rings.charAt(i)).add(rings.charAt(i-1));
+        }
+            
+        for(Set<Character> x: map1.values())
+            if (x.size()==3) count++;
+        return count; 
+    }
+}
